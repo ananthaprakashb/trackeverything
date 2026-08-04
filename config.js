9b051c3b8ed77d8992594b9cc29bda0925cd0a33
Ananthaprakash <ananthaprakashb@gmail.com>
2026-08-03 20:44:59 -0700

    Load PWA controller

--- config.js
+++ config.js
@@ -2,3 +2,8 @@
 window.TRACK_EVERYTHING_CONFIG = {
   apiUrl: "https://track-everything-api-854374277452.us-west1.run.app"
 };
+
+const pwaScript = document.createElement('script');
+pwaScript.src = 'assets/pwa.js';
+pwaScript.defer = true;
+document.head.appendChild(pwaScript);
